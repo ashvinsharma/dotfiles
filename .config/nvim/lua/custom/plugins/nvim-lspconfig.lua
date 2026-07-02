20eb1369a61b97b05341f6f8522e0f3685821172
Ashvin Sharma <ashvinsharma97@gmail.com>
2026-07-02 18:31:14 +0530

    feat(nvim): add postgres-language-server LSP and outline.nvim plugin
    postgres-language-server wired into lspconfig's servers table. outline.nvim
    added lazily on :Outline/:OutlineOpen, bound to <leader>o.

--- .config/nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -195,6 +195,10 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
+      ['postgres-language-server'] = {
+        cmd = { 'postgres-language-server', 'lsp-proxy' },
+      },
+
       gopls = {},
 
       yamlls = {},
