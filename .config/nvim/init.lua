99478a52f7fa69383060ddf62dcbee403993e9ff
Ashvin Sharma <ashvinsharma97@gmail.com>
2026-07-13 02:35:23 +0530

    feat(nvim): add cfilter pack

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- Helps filter results from quickfix list
+vim.cmd 'packadd cfilter'
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
