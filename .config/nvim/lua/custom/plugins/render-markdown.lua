69fe3571144857ca364cc3b08cfac5386136e340
Ashvin Sharma <ashvinsharma97@gmail.com>
2026-07-14 21:29:07 +0530

    feat(nvim): customize render-markdown checkbox icons

--- .config/nvim/lua/custom/plugins/render-markdown.lua
+++ .config/nvim/lua/custom/plugins/render-markdown.lua
@@ -1,6 +1,21 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   ft = { 'markdown' },
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   opts = {},
+  config = function()
+    require('render-markdown').setup {
+      checkbox = {
+        checked = {
+          icon = '✓',
+        },
+        custom = {
+          todo = {
+            raw = '[-]',
+            rendered = '⏲︎',
+          },
+        },
+      },
+    }
+  end,
 }
