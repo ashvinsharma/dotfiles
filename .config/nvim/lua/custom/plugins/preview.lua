46f680d7f898bf9a5fa48c5e814c359bd415c627
Ashvin Sharma <ashvinsharma97@gmail.com>
2026-07-16 04:27:10 +0530

    fix(nvim): open markdown preview via :MarkdownPreview, default to Arc
    The keymap called a `previewers[vim.bo.filetype]` dispatch table that
    isn't defined anywhere in this file -- dead/broken code. Moved the
    keymap into config() to call :MarkdownPreview directly.

--- .config/nvim/lua/custom/plugins/preview.lua
+++ .config/nvim/lua/custom/plugins/preview.lua
@@ -1,19 +1,14 @@
-vim.keymap.set('n', '<leader>tp', function()
-  local fn = previewers[vim.bo.filetype]
-  if not fn then
-    vim.notify('No preview available for filetype: ' .. vim.bo.filetype, vim.log.levels.WARN)
-    return
-  end
-  fn()
-end, { desc = '[T]oggle [P]review' })
-
 return {
   {
     'selimacerbas/markdown-preview.nvim',
     dependencies = { 'selimacerbas/live-server.nvim' },
     ft = { 'markdown' },
     config = function()
-      require('markdown_preview').setup {}
+      require('markdown_preview').setup {
+        browser = 'Arc',
+      }
+
+      vim.keymap.set('n', '<leader>tp', '<cmd>MarkdownPreview<CR>', { desc = '[T]oggle [P]review' })
     end,
   },
 }
